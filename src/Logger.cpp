5f45521591b2f78ae182488c7cf47050072ce8c1
Anthony Sanchez <anthony.sanchezquiros@ucr.ac.cr>
2026-01-20 20:31:06 -0600

    test: adding test to Logger code

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -3,18 +3,27 @@
 #include <iostream>
 
 Reservation Logger::parseJson(const std::string& jsonFile) {
-    boost::json::object currentJson = boost::json::parse(jsonFile).as_object();
     Reservation currentReservation;
-    // we take all the json information, if someting went wrong, returns nullptr
+    
     try {
+        // parsing json
+        boost::json::object currentJson = boost::json::parse(jsonFile).as_object();
+        // get all the json information about the reservaton
         currentReservation.owner = currentJson.at("owner").as_string();
         currentReservation.expirationDate = currentJson.at("expirationDate").as_string();
         currentReservation.category = currentJson.at("category").as_string();
         currentReservation.cost = currentJson.at("cost").as_int64();
         currentReservation.room = currentJson.at("room").as_int64();
+        // if any data of the reservation is invalid/void, throws a invalid argument err
+        if (!validateJsonFormat(currentReservation)) {
+            throw std::invalid_argument("Invalid reservation format");
+        }
+    } catch (const std::invalid_argument&) {
+        throw;
     } catch (const std::exception& e) {
         throw std::invalid_argument("JSON parsing failed: " + std::string(e.what()));
     }
+    
     return currentReservation;
 };
 
---
+++ tests/LoggerTest.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include "../src/Logger.hpp"
+
+TEST(Logger, FunctionalJson) {
+    std::string validJson = R"({
+        "owner": "Juan",
+        "expirationDate": "2026-01-25",
+        "category": "deluxe",
+        "cost": 150,
+        "room": 101
+    })";
+    Logger logger;
+    Reservation res = logger.parseJson(validJson);
+    EXPECT_EQ(res.owner, "Juan");
+    EXPECT_EQ(res.expirationDate, "2026-01-25");
+    EXPECT_EQ(res.category, "deluxe");
+    EXPECT_EQ(res.cost, 150);
+    EXPECT_EQ(res.room, 101);
+}
+
+TEST(Logger, InvalidJson) {
+    std::string invalidJson = R"({
+        "owner": "Juan"
+        // Falta expirationDate, category, etc
+    })";
+    Logger logger;
+    EXPECT_THROW(logger.parseJson(invalidJson), std::invalid_argument);
+    std:: string voidJson = "";
+    EXPECT_THROW(logger.parseJson(voidJson), std::invalid_argument);
+    std:: string duplicateJson = R"({
+        "owner": "Juan",
+        "expirationDate": "2026-01-25",
+        "category": "deluxe",
+        "cost": 150,
+        "room": 101
+        "owner": "Juan2",
+        "expirationDate": "2026-01-25",
+        "category": "deluxe2",
+        "cost": 1500,
+        "room": 1010
+    })";
+    EXPECT_THROW(logger.parseJson(duplicateJson), std::invalid_argument);
+
+    std:: string twoOwnersJson = R"({
+        "owner": "Juan, Daniel",
+        "expirationDate": "2026-01-25",
+        "category": "deluxe",
+        "cost": 150,
+        "room": 101
+    })";
+}
